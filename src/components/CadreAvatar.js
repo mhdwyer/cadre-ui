25a2494003440b5a9b61bb5cadec7fedee5f9dac
Michael Dwyer <mhdwyer@hotmail.com>
2020-09-06 21:05:10 -0500

    Updating to use Gravatar

--- src/components/CadreAvatar.js
+++ src/components/CadreAvatar.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import avatarCurves1 from '../assets/curve1.svg';
 import './CadreAvatar.css'
-import personImage from '../assets/michaeldwyer.png';
+import Gravatar from 'react-gravatar'
 
 export default function CadreAvatar(props) {
     return (
       <div className="avatar" style={{top:props.positionTop,left:props.positionLeft}} >
-        <img className="personImage" src={personImage} width="40" />
+        {/* <img className="personImage" src={personImage} width="40" /> */}
+        <Gravatar className="personImage" email={props.email} size="40"/> 
         <img className="avatarCurves" src={avatarCurves1} width="45" /><br/>
-        <div className="avatarNameLabel">Michael Dwyer</div>
+        <div className="avatarNameLabel">{props.name}</div>
         <svg className="beacon" width="40" height="40">
           <circle cx="50%" cy="50%" r="7px" fill="orange"></circle>
           <circle className="pulse" cx="50%" cy="50%" r="10px" fill="orange"></circle>
--- src/components/CadreEntry.js
+++ src/components/CadreEntry.js
@@ -8,8 +8,8 @@ export default function CadreEntry({match}) {
       <div className="container">  ..
         <div className="floorArea">
           <img src={floorPlan} /> x/.
-          <CadreAvatar positionTop={58} positionLeft={360} />
-          <CadreAvatar positionTop={250} positionLeft={380} />
+          <CadreAvatar positionTop={58} positionLeft={360} name="Jennifer Dwyer" email="jdwyercpa@hotmail.com"/>
+          <CadreAvatar positionTop={250} positionLeft={380} name="Michael Dwyer" email="mhdwyer@hotmail.com"/>
         </div>
       </div>
     );
